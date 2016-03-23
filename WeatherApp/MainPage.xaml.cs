1ddea9122650bbb151b66f43a0cd9ff605c2a3c3
nvsap <nvsap_@outlook.com>
2016-03-23 17:48:35 +0200

    Commit#1

--- WeatherApp/MainPage.xaml.cs
+++ WeatherApp/MainPage.xaml.cs
@@ -4,9 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using WeatherNet;
-using WeatherNet.Clients;
-using WeatherNet.Model;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -18,8 +15,10 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Xml;
 using System.Xml.Linq;
-//using System.Web;
-
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using Windows.Data.Json;
 
 namespace WeatherApp
 {
@@ -28,27 +27,57 @@ namespace WeatherApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        SingleResult<CurrentWeatherResult> result;
+        JsonObject o;
         public MainPage()
         {
             this.InitializeComponent();
         }
 
+        private async void getMsg()
+        {
+            string url = /*@*/"http://api.wunderground.com/api/a6743ab79712c4f3/forecast/lang:UA/q/zmw:00000.1.33345.json";
+            var client = new HttpClient();
+            HttpResponseMessage msg = await client.GetAsync(new Uri(url));
+            var json = await msg.Content.ReadAsStringAsync();
+            o = JsonValue.Parse(json).GetObject();
+        }
+
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            XmlDocument doc = new XmlDocument();
-            String st = String.Format(@"http://api.wunderground.com/api/a6743ab79712c4f3/forecast/lang:UA/q/zmw:00000.1.33345.json");
-            doc.LoadXml(st);
-            textBlock.Text = doc.ToString();
-            //try
+            // string url = /*@*/"http://api.wunderground.com/api/a6743ab79712c4f3/forecast/lang:UA/q/zmw:00000.1.33345.xml";
+            //XmlDocument doc = new XmlDocument();
+
+            //doc.LoadXml("http://api.wunderground.com/api/a6743ab79712c4f3/forecast/lang:UA/q/zmw:00000.1.33345.xml");
+
+            //var clinet = new HttpClient();
+
+            //JsonObject o = null;
+            getMsg();
+            textBlock.Text = o != null ? o.ToString() : "Null!!!";
+
+            //HttpResponseMessage msg = await client.GetAsync(new Uri(url));
+
+            //textBlock.Text = doc.ToString();
+           /* var m_strFilePath = "http://api.wunderground.com/api/a6743ab79712c4f3/forecast/lang:UA/q/zmw:00000.1.33345.xml";
+            string xmlStr;
+        
+            using (var wc = new WebClient())
+            {
+                xmlStr = wc.DownloadString(m_strFilePath);
+            }
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlStr);*/
+
+            //string s = "";
+            //while(reader.Read())
             //{
-            //    //doc.LoadXml(st);
-            //    textBlock.Text = /*doc.ToString()*/st;
+            // s += reader.Name;
+            // s += "\n";
             //}
-            //catch(System.Exception ex)
-            //{
-            //    textBlock.Text = ex.ToString();
-            //}
+
+            //textBlock.Text = s;
         }
+        
     }
+
 }
