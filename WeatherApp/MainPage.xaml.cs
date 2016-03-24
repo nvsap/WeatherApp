686ee820a834e128865c24987673e9d081b9ee7c
nvsap <nvsap_@outlook.com>
2016-03-24 22:02:34 +0200

    commit#3

--- WeatherApp/MainPage.xaml.cs
+++ WeatherApp/MainPage.xaml.cs
@@ -18,66 +18,92 @@ using System.Xml.Linq;
 using System.Text;
 using System.Net;
 using System.Net.Http;
-using Windows.Data.Json;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace WeatherApp
 {
-    /// <summary>
-    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
-        JsonObject o;
+        XmlDocument doc = new XmlDocument();
+
         public MainPage()
         {
             this.InitializeComponent();
+            getWeather();
         }
 
-        private async void getMsg()
+        private async void getWeather()
         {
-            string url = /*@*/"http://api.wunderground.com/api/a6743ab79712c4f3/forecast/lang:UA/q/zmw:00000.1.33345.json";
+            string url = "http://api.wunderground.com/api/a6743ab79712c4f3/geolookup/forecast10day/lang:UA/q/autoip.xml";
             var client = new HttpClient();
             HttpResponseMessage msg = await client.GetAsync(new Uri(url));
-            var json = await msg.Content.ReadAsStringAsync();
-            o = JsonValue.Parse(json).GetObject();
+            var xml = await msg.Content.ReadAsStringAsync();
+            doc.LoadXml(xml);
+            WeatherDataStructures.Forecast7Days weather = DataHelper.ProcessXML(doc);
+            FillLables(weather);
         }
-
-        private void button_Click(object sender, RoutedEventArgs e)
+        
+        private void FillLables(WeatherDataStructures.Forecast7Days weather)
         {
-            // string url = /*@*/"http://api.wunderground.com/api/a6743ab79712c4f3/forecast/lang:UA/q/zmw:00000.1.33345.xml";
-            //XmlDocument doc = new XmlDocument();
+            this.TextLoc.Text = weather.Location;
 
-            //doc.LoadXml("http://api.wunderground.com/api/a6743ab79712c4f3/forecast/lang:UA/q/zmw:00000.1.33345.xml");
+            this.CurrDay.Text = weather.Forecast[0].Day;
+            this.DayImg1.Source = new BitmapImage(new Uri(weather.Forecast[0].IconURL, UriKind.Absolute));
+            this.DayHigh1.Text = weather.Forecast[0].Max.Celsius;
+            this.NightHigh1.Text = weather.Forecast[0].Min.Celsius;
+            this.TextWeather1.Text = weather.Metric;
 
-            //var clinet = new HttpClient();
+            this.DayImg2.Source = new BitmapImage(new Uri(weather.Forecast[1].IconURL, UriKind.Absolute));
+            this.TempDay1.Text = weather.Forecast[1].Max.Celsius;
+            this.TempNight1.Text = weather.Forecast[1].Min.Celsius;
+            this.textDay1.Text = weather.Forecast[1].Day;
+            this.TextCond1.Text = weather.Forecast[1].Condition;
 
-            //JsonObject o = null;
-            getMsg();
-            textBlock.Text = o != null ? o.ToString() : "Null!!!";
+            this.DayImg3.Source = new BitmapImage(new Uri(weather.Forecast[2].IconURL, UriKind.Absolute));
+            this.TempDay2.Text = weather.Forecast[2].Max.Celsius;
+            this.TempNight2.Text = weather.Forecast[2].Min.Celsius;
+            this.textDay2.Text = weather.Forecast[2].Day;
+            this.TextCond2.Text = weather.Forecast[2].Condition;
 
-            //HttpResponseMessage msg = await client.GetAsync(new Uri(url));
+            this.DayImg4.Source = new BitmapImage(new Uri(weather.Forecast[3].IconURL, UriKind.Absolute));
+            this.TempDay3.Text = weather.Forecast[3].Max.Celsius;
+            this.TempNight3.Text = weather.Forecast[3].Min.Celsius;
+            this.textDay3.Text = weather.Forecast[3].Day;
+            this.TextCond3.Text = weather.Forecast[3].Condition;
 
-            //textBlock.Text = doc.ToString();
-           /* var m_strFilePath = "http://api.wunderground.com/api/a6743ab79712c4f3/forecast/lang:UA/q/zmw:00000.1.33345.xml";
-            string xmlStr;
-        
-            using (var wc = new WebClient())
-            {
-                xmlStr = wc.DownloadString(m_strFilePath);
-            }
-            var xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(xmlStr);*/
+            this.DayImg5.Source = new BitmapImage(new Uri(weather.Forecast[4].IconURL, UriKind.Absolute));
+            this.TempDay4.Text = weather.Forecast[4].Max.Celsius;
+            this.TempNight4.Text = weather.Forecast[4].Min.Celsius;
+            this.textDay4.Text = weather.Forecast[4].Day;
+            this.TextCond4.Text = weather.Forecast[4].Condition;
 
-            //string s = "";
-            //while(reader.Read())
-            //{
-            // s += reader.Name;
-            // s += "\n";
-            //}
+            this.DayImg6.Source = new BitmapImage(new Uri(weather.Forecast[5].IconURL, UriKind.Absolute));
+            this.TempDay5.Text = weather.Forecast[5].Max.Celsius;
+            this.TempNight5.Text = weather.Forecast[5].Min.Celsius;
+            this.textDay5.Text = weather.Forecast[5].Day;
+            this.TextCond5.Text = weather.Forecast[5].Condition;
 
-            //textBlock.Text = s;
+            this.DayImg7.Source = new BitmapImage(new Uri(weather.Forecast[6].IconURL, UriKind.Absolute));
+            this.TempDay6.Text = weather.Forecast[6].Max.Celsius;
+            this.TempNight6.Text = weather.Forecast[6].Min.Celsius;
+            this.textDay6.Text = weather.Forecast[6].Day;
+            this.TextCond6.Text = weather.Forecast[6].Condition;
         }
-        
+
+        private void OnLangClick(object sender, RoutedEventArgs e)
+        {
+            //this.DayImg1.Source = new BitmapImage(new Uri("http://icons.wxug.com/i/c/k/snow.gif", UriKind.Absolute));
+        }
+
+        private void MenuButton1_Click(object sender, RoutedEventArgs e)
+        {
+            //this.DayImg1.Source = new BitmapImage(new Uri("http://icons.wxug.com/i/c/k/partlycloudy.gif", UriKind.Absolute));
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            getWeather();
+        }
     }
 
 }
---
+++ WeatherApp/Weather Data/WeatherDataStructures.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    class WeatherDataStructures
+    {
+        public struct forecast
+        {
+            string text;
+            SimpleForecastData[] forecastWeek;
+        }
+
+        public struct Forecast7Days
+        {
+            string location;
+            string textImperial;
+            string textMetric;
+            SimpleForecastData[] forecastWeek;
+
+            public string Location { get { return location; } set { location = value; } }
+            public string Imperial { get { return textImperial; } set { textImperial = value; } }
+            public string Metric { get { return textMetric; } set { textMetric = value; } }
+            public SimpleForecastData[] Forecast { get { return forecastWeek; } set { forecastWeek = value; } }
+
+            public Forecast7Days(string location, string imperial, string metric, SimpleForecastData[] forecast)
+            {
+                this.location = location;
+                this.textImperial = imperial;
+                this.textMetric = metric;
+                this.forecastWeek = forecast;
+            }
+        }
+
+        public struct SimpleForecastData
+        {
+            //Date date;
+            string day;
+            Temperature max;
+            Temperature min;
+            string condition;
+            string iconName;
+            string iconURL;
+            //Wind maxWind;
+            //Wind averageWind;
+
+            public string Day { get { return day; } set { day = value; } }
+            public Temperature Max { get { return max; } set { max = value; } }
+            public Temperature Min { get { return min; } set { min = value; } }
+            public string Condition { get { return condition; } set { condition = value; } }
+            public string IconName { get { return iconName; } set { iconName = value; } }
+            public string IconURL { get { return iconURL; } set { iconURL = value; } }
+            //public Wind MaxWind { get { return maxWind; } set { maxWind = value; } }
+            //public Wind AverageWind { get { return averageWind; } set { averageWind = value; } }
+
+            public SimpleForecastData(/*Date date*/string day, Temperature max, Temperature min, string condition, string iconName, string iconURL/*, Wind maxWind, Wind averageWind*/)
+            {
+                this.day = day;
+                this.max = max;
+                this.min = min;
+                this.condition = condition;
+                this.iconName = iconName;
+                this.iconURL = iconURL;
+                //this.maxWind = maxWind;
+                //this.averageWind = averageWind;
+            }
+        }
+
+        public struct Date
+        {
+            string day { get; set; }
+            string month;
+            string year;
+            string monthName;
+            string monthNameShort;
+            string weekDay;
+            string weekDayShort;
+        }
+
+        public struct Temperature
+        {
+            string celsius;
+            string fahrenheit;
+
+            public string Celsius { get { return celsius; } set { celsius = value; } }
+            public string Fahrenheit { get { return fahrenheit; } set { fahrenheit = value; } }
+
+
+        }
+
+        public struct Wind
+        {
+            string kph;
+            string mph;
+            string dir;
+            string degreed;
+
+            public string KpH { get { return kph; } set { kph = value; } }
+            public string MpH { get { return mph; } set { mph = value; } }
+            public string Direction { get { return dir; } set { dir = value; } }
+            public string Degreed { get { return degreed; } set { degreed = value; } }
+
+        }
+
+        public struct Height
+        {
+            string cm;
+            string inch;
+        }
+    }
+}
